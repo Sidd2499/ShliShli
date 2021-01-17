460509800559e22fd856a6f473cb736df44eda6b
Sidd2499 <kishannowal@gmail.com>
2021-01-17 12:49:58 +0530

    Random Commit

---
+++ app/src/main/java/com/example/shlishli/ItemDisplayActivity.java
@@ -0,0 +1,14 @@
+package com.example.shlishli;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ItemDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_display);
+    }
+}
